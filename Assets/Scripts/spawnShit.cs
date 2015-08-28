6d79434374ae209940b519415a7fe527f23be52f
Gerardo Galvez <gerardogv96@hotmail.com>
2015-08-27 19:47:11 -0500

    Add Obstacle prefab and script to spawn them

---
+++ Assets/Scripts/spawnShit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnShit : MonoBehaviour {
+	private Camera cam;
+	public GameObject shit;
+	private Vector3 pos;
+	// Use this for initialization
+	void Start () {
+		pos = new Vector3 (0, -5, 0);
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Move camera upwards
+		cam.transform.Translate (0, 0.01f, 0);
+		if (Input.GetKeyDown ("space")) {
+			//Instantiate obstacle between minimum and maximum position
+			pos.x = (float)Random.Range(-1f, 3f);
+			Instantiate(shit, pos, Quaternion.identity);
+
+			//gg
+			pos.y+=2.5f;;
+		}
+	}
+}
