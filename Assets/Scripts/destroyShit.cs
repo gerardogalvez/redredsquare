263c7517679cb7b928cee870d5b2d85687e21f23
Gerardo Galvez <gerardogv96@hotmail.com>
2015-09-08 22:34:50 -0500

    Add font and script to destroy obstacles when out of sight

---
+++ Assets/Scripts/destroyShit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyShit : MonoBehaviour {
+
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cam.transform.position.y - this.transform.position.y > 7)
+			Destroy (this.gameObject);
+	}
+}
