47ee97642d58014603dde73d732ecc823febd952
Gerardo Galvez <gerardogv96@hotmail.com>
2015-09-20 19:11:32 -0500

    Fade out main menu

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,22 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour {
 
     public GameObject scoreText;
     public GameObject mainmenuUI;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         Time.timeScale = 0;
         scoreText.SetActive(false);
         mainmenuUI.SetActive(true);
-	}
-	
-	public void Play()
+    }
+
+    public void Play()
     {
-        mainmenuUI.SetActive(false);
+        StartCoroutine("fade");
+        //mainmenuUI.SetActive(false);
         scoreText.SetActive(true);
         Time.timeScale = 1;
+    }
+
+    IEnumerator fade()
+    {
+        CanvasGroup canvasGroup = mainmenuUI.GetComponent<CanvasGroup>();
+        while(canvasGroup.alpha>0)
+        {
+            canvasGroup.alpha -= 0.05f;
+            yield return null;
+        }
+        mainmenuUI.SetActive(false);
+        yield return null;
     }
 }
